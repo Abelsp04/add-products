9dc2eb9fa23937c3ebe4e0f04a41b0e909827b63
Abel Segui <Abelsegui@hotmail.com>
2020-05-07 03:26:51 +0000

    secound_commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,33 +6,45 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<div className="card">
-				<div className="card-header">
-					<h4>Add Products</h4>
+		<div className="container">
+			<div className="row pt-5">
+				<div className="col-md-4">
+					<div className="card">
+						<div className="card-header">
+							<h4>Add Products</h4>
+						</div>
+						<form className="card-body">
+							<div className="form-group">
+								<input
+									className="form-control"
+									id="name"
+									type="text"
+									placeholder="Product Name"
+								/>
+								<input
+									className="form-control"
+									id="price"
+									type="number"
+									step="0.01"
+									placeholder="Product Price"
+								/>
+								<input
+									className="form-control"
+									id="year"
+									type="number"
+									placeholder="Year Product"
+								/>
+							</div>
+							<input
+								className="btn btn-primary btn-block"
+								type="submit"
+								value="save"
+							/>
+						</form>
+					</div>
 				</div>
-				<div className="card-body">
-					<input
-						className="form-control"
-						id="name"
-						type="text"
-						placeholder="Product Name"
-					/>
-					<input
-						className="form-control"
-						id="price"
-						type="number"
-						placeholder="Product Price"
-					/>
-					<input
-						className="form-control"
-						id="year"
-						type="number"
-						placeholder="Year Product"
-					/>
-					<a href="#" className="btn btn-primary">
-						Go somewhere
-					</a>
+				<div className="col-md-8">
+					<div className="ptoduct-list" />
 				</div>
 			</div>
 		</div>
