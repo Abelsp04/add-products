5f7c25464086cc3af4bd5252621f0ca6ec29e180
Abel Segui <Abelsegui@hotmail.com>
2020-05-10 01:18:17 +0000

    styles_not_ready

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,9 @@ import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
-
+// import * as serviceWorker from "./serviceWorker";
 //import your own components
-import { Home } from "./component/home.js";
+import { App } from "./component/app.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
